cd16127d7fd6ee8b963f2808d10710d2fea8345b
Gocy <caiyang@yy.com>
2018-01-26 17:15:49 +0800

    fix test require(path)

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert')
-var validator = require('gocy-validator')
+var validator = require('../../gocy-validator')
 
 describe('Validator', function () {
     describe('#containsOnlyEnglish', function () {
