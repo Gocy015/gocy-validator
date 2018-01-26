4e9100955f09f2b31fa1bf88110631c2e8c3f9d6
Gocy <caiyang@yy.com>
2018-01-26 16:33:29 +0800

    initial commit

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert')
+var validator = require('gocy-validator')
+
+describe('Validator', function () {
+    describe('#containsOnlyEnglish', function () {
+        it('should return true when a string contains only english characters', function () {
+            if (validator.containsOnlyEnglish('Aabc') == false) {
+                throw new Error('Validator#containsOnlyEnglish is not right')
+            }
+        })
+    })
+})
